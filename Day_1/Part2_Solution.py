9f60b6815c0bdf1487586bbe07ba852e54cd83b4
Rafae Pasha <8557992+RafaePasha@users.noreply.github.com>
2024-12-01 16:36:47 -0800

    challenge part 2 complete

--- Day_1/Part2_Solution.py
+++ Day_1/Part2_Solution.py
@@ -1,4 +1,20 @@
-def main()
+from collections import Counter
+
+
+def main():
+    file_path = r"C:\Users\Rafae\Code\AdventOfCode_2024\Day_1\input.txt"
+
+    with open(file_path, "r") as file:
+        lines = file.readlines()
+
+    list1, list2 = zip(*(map(int, line.strip().split("   "))
+                       for line in lines if line.strip()))
+
+    list2_counter = Counter(list2)
+
+    similarity_score = sum(val * list2_counter[val] for val in list1)
+
+    print(similarity_score)
 
 
 if __name__ == "__main__":
