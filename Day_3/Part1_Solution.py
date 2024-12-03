a0c3be6267ad55d95b5a0f3c1855094c207265ce
Rafae Pasha <8557992+RafaePasha@users.noreply.github.com>
2024-12-02 21:28:14 -0800

    completed day 3

--- Day_3/Part1_Solution.py
+++ Day_3/Part1_Solution.py
@@ -1,4 +1,17 @@
+import re
+
+
 def main():
+    total_sum = 0
+    pattern = re.compile(r'mul\((\d+),\s*(\d+)\)')
+
+    with open(r'C:\Users\Rafae\Code\AdventOfCode_2024\Day_3\input.txt', 'r') as file:
+        for line in file:
+            matches = pattern.findall(line)
+            for match in matches:
+                total_sum += int(match[0]) * int(match[1])
+
+    print(total_sum)
 
 
 if __name__ == "__main__":
--- Day_3/Part2_Solution.py
+++ Day_3/Part2_Solution.py
@@ -1,4 +1,30 @@
+import re
+
+
 def main():
+    total_sum = 0
+    mul_enabled = True
+    mul_pattern = re.compile(r'mul\((\d+),\s*(\d+)\)')
+    do_pattern = re.compile(r'do\(\)')
+    dont_pattern = re.compile(r"don't\(\)")
+
+    file_path = r'C:\Users\Rafae\Code\AdventOfCode_2024\Day_3\input.txt'
+    with open(file_path, 'r') as file:
+        content = file.read()
+
+    parts = re.split(r'(do\(\)|don\'t\(\))', content)
+    for part in parts:
+        if do_pattern.fullmatch(part):
+            mul_enabled = True
+        elif dont_pattern.fullmatch(part):
+            mul_enabled = False
+        elif mul_enabled:
+            matches = mul_pattern.findall(part)
+            for match in matches:
+                if len(match) == 2:
+                    total_sum += int(match[0]) * int(match[1])
+
+    print(total_sum)
 
 
 if __name__ == "__main__":
