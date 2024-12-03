31167c301acc08f86af29fc84a763d4f524ffc0b
Rafae Pasha <8557992+RafaePasha@users.noreply.github.com>
2024-12-02 21:30:52 -0800

    cleaned up part 1 a little

--- Day_3/Part1_Solution.py
+++ Day_3/Part1_Solution.py
@@ -2,14 +2,14 @@ import re
 
 
 def main():
-    total_sum = 0
     pattern = re.compile(r'mul\((\d+),\s*(\d+)\)')
+    file_path = r'C:\Users\Rafae\Code\AdventOfCode_2024\Day_3\input.txt'
 
-    with open(r'C:\Users\Rafae\Code\AdventOfCode_2024\Day_3\input.txt', 'r') as file:
-        for line in file:
-            matches = pattern.findall(line)
-            for match in matches:
-                total_sum += int(match[0]) * int(match[1])
+    with open(file_path, 'r') as file:
+        content = file.read()
+
+    total_sum = sum(int(match[0]) * int(match[1])
+                    for match in pattern.findall(content))
 
     print(total_sum)
 
